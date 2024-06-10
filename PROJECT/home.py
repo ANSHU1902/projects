bc4be20debcabb3e76479b01365be43ea62b8dde
ANSHU1902 <162030015+ANSHU1902@users.noreply.github.com>
2024-06-10 11:46:49 +0530

    Add files via upload

---
+++ PROJECT/home.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+def app():
+    st.title(':red[PROFITOLIO] :cry:')
+    st.subheader('A Portfolio Management System')
+
---
+++ PROJECT/portfolio.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+def app():
+
+    st.title('welcome to :violet[Portfolios] :smile:')
+    
